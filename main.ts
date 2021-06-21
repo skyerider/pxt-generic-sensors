86728fbf80f7eb54510de6d580cf796fefd9d561
skyerider <skyerider777@hotmail.com>
2021-06-21 10:29:05 +0800

    修复LCD1602的一个方法

--- main.ts
+++ main.ts
@@ -1001,8 +1001,7 @@ namespace makerbit {
       }
   }
 
-
-  //% block="LCD1602显示屏控制"   group="LCD1602显示屏"  
+  //% block="i2cLcdDisplay_Control %item"   group="LCD1602显示屏"  
   //% subcategory="输出模块"
   //% weight=64
   export function i2cLcdDisplay_Control(item: Item): void {
